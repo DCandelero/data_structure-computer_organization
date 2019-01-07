8a26897fefbc5a34ea9ad5fd7c70458779367717
DCandelero <davidcandelero557@gmail.com>
2019-01-07 06:28:52 +0000

    ideal pipeline

--- computer_organization/main.c
+++ computer_organization/main.c
@@ -1,17 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int runClock() {
+    int clock = 0;
+    int i = 0;
+    int line[20];
+    int aux = 0;
+    int option = 1;
 
-int main() {
-    int option = 0;
-
-    printf()
-
-    while (option != -1) {
-        option = getchar();
+    //filling array with zeros
+    for (int j = 0; j < 20; j++) {
+        line[j] = 0;
     }
 
+    while (option) {
+        clock++;
+        i = aux;
+        while (i < clock) {
+            line[i]++;
 
+            //6 stages
+            if (line[i] == 6) {
+                aux = i;
+            }
+
+            i++;
+        }
+
+        for (int j = 0; j < clock; j++) {
+            switch (line[j]) {
+                case 1:
+                    printf(" FI ");
+                    break;
+                case 2:
+                    printf(" DI ");
+                    break;
+                case 3:
+                    printf(" CO ");
+                    break;
+                case 4:
+                    printf(" FO ");
+                    break;
+                case 5:
+                    printf(" EI ");
+                    break;
+                case 6:
+                    printf(" WO ");
+                    break;
+                default:
+                    printf("    ");
+            }
+        }
+        printf("\n");
+
+        option = (getchar() == '\n') ? 1 : 0;
+    }
+
+}
+
+int main() {
+
+    runClock();
 
 
     return 0;
